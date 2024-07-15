1ca81706601be2aa644edfe62945c461d97a8092
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-07-15 16:42:43 +0300

    feat: made other endpt but json decoding has a problem

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	// "time"
 )
 
 func getNote(w http.ResponseWriter, req *http.Request) {
@@ -18,8 +20,33 @@ func getNote(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, "helloooooooooo")
 }
 
+type Note struct {
+	Id      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+func createNote(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		fmt.Fprint(w, "Error method of '/createNote' request should be POST!")
+		return
+	}
+	str, _ := io.ReadAll(req.Body)
+	var note Note
+	// fmt.Println(string(str))
+	// err := json.NewDecoder(req.Body).Decode(&note)
+	err := json.Unmarshal(str, &note)
+	if err != nil {
+		fmt.Println("Error decoding")
+	}
+	fmt.Println(note)
+	// fmt.Println(note.Id)
+	// fmt.Println(note.Title)
+}
+
 func main() {
 	http.HandleFunc("/getNote", getNote)
+	http.HandleFunc("/createNote", createNote)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("error detected while reading request body")
