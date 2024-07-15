ab8a10376076d4b6cb18ebdb70825529c7d5ec6b
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-07-15 20:13:58 +0300

    fix: handling bad requests from user

--- backend/server/server.go
+++ backend/server/server.go
@@ -6,19 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
-	// "time"
 )
-
-func getNote(w http.ResponseWriter, req *http.Request) {
-	_, err := io.ReadAll(req.Body)
-	if err != nil {
-		fmt.Println("error detected while reading request body")
-		os.Exit(1)
-	}
-	// str := string(body)
-	// fmt.Println(string(body))
-	fmt.Fprint(w, "helloooooooooo")
-}
 
 type Note struct {
 	Title   string `json:"title"`
@@ -30,9 +18,42 @@ func NewNote() Note {
 	return Note{}
 }
 
+func getNote(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Error method of '/getNote' request should be GET!")		
+		return
+	}
+	_, err := io.ReadAll(req.Body)
+	if err != nil {
+		fmt.Println("error detected while reading request body")
+		os.Exit(1)
+	}
+	// str := string(body)
+	// fmt.Println(string(body))
+	fmt.Fprint(w, "helloooooooooo")
+}
+
+func getNotes(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Error method of '/getNotes' request should be GET!")		
+		return
+	}
+	_, err := io.ReadAll(req.Body)
+	if err != nil {
+		fmt.Println("error detected while reading request body")
+		os.Exit(1)
+	}
+	// str := string(body)
+	// fmt.Println(string(body))
+	fmt.Fprint(w, "helloooooooooo")
+}
+
 func createNote(w http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
-		fmt.Fprint(w, "Error method of '/createNote' request should be POST!")
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Error method of '/createNote' request should be POST!")		
 		return
 	}
 	str, _ := io.ReadAll(req.Body)
@@ -46,8 +67,11 @@ func createNote(w http.ResponseWriter, req *http.Request) {
 	fmt.Println(note)
 }
 
+
+
 func main() {
 	http.HandleFunc("/getNote", getNote)
+	http.HandleFunc("/getNotes", getNotes)
 	http.HandleFunc("/createNote", createNote)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
