9c665165ea52e6dcad05fc820c29eb716aa9e2a8
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-07-15 14:07:19 +0300

    feat: called the listenAndServe function and checked it works

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,5 +1,28 @@
-package server
+package main
 
 import (
-// "fmt"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
+
+func getNote(w http.ResponseWriter, req *http.Request) {
+	_, err := io.ReadAll(req.Body)
+	if err != nil {
+		fmt.Println("error detected while reading request body")
+		os.Exit(1)
+	}
+	// str := string(body)
+	// fmt.Println(string(body))
+	fmt.Fprint(w, "helloooooooooo")
+}
+
+func main() {
+	http.HandleFunc("/getNote", getNote)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("error detected while reading request body")
+		os.Exit(1)
+	}
+}
